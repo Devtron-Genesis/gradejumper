0755b718f994ec691307b5772c0be1db61146598
Unknown <deanhopkins@gmail.com>
2019-04-02 11:23:41 +0100

    Trello #299

--- sites/all/modules/gj_deanhopkins_tutor_ad/js/gj_deanhopkins_tutor_ad_autoupload.js
+++ sites/all/modules/gj_deanhopkins_tutor_ad/js/gj_deanhopkins_tutor_ad_autoupload.js
@@ -16,13 +16,28 @@
             $('button.form-submit[value=Upload]', $parent).mousedown();
             $('#tutor-ad-uploading').css("display","block");
 
-            setTimeout(function(){
-                $('#tutor-ad-uploading').css("display","none");
-            }, 2500);
+            var waitForUpload = setInterval(function(){
+              if (document.getElementById("edit-field-tutor-upload-profile-image-und-0-ajax-wrapper").children[0].children[1].children[2].children.length > 1){
+                try {
+                  setImagePreview();
+                  $('#tutor-ad-uploading').css("display","none");
+                  clearInterval(waitForUpload);
+                } catch (err){
+                  print("not ready");
+                }
+              }
+
+            }, 100);
 
           }
-        }, 100);
+        }, 500);
       });
     }
   };
 })(jQuery);
+
+function setImagePreview(){
+  var url = document.getElementById("edit-field-tutor-upload-profile-image-und-0-ajax-wrapper").children[0].children[1].children[2].children[1].getAttribute("href");
+  document.getElementById("edit-field-tutor-upload-profile-image-und-0-ajax-wrapper").children[0].children[1].children[2].children[0].style.display = "none";
+  document.getElementById("edit-field-tutor-upload-profile-image-und-0-ajax-wrapper").children[0].children[1].children[2].children[1].innerHTML = '<div class="user-picture"><img style="height: 100%; object-fit: cover;" src="' + url + '"></img></div>';
+}
